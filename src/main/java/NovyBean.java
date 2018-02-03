4808873753a75232f39a6d5913279c4668e8f2e2
Archlin <arch@clarity.lan>
2018-02-04 00:27:54 +0100

    cucanie opisu filmu 5

--- src/main/java/NovyBean.java
+++ src/main/java/NovyBean.java
@@ -100,7 +100,7 @@ public class NovyBean implements Serializable {
             return;
         }
         setLog("ideme natiahnut link "+zvoleny);
-        film = ejb.nacitajFilm(zvoleny);
+        film = ejb.nacitajFilm("https://www.csfd.cz"+zvoleny);
         if (film!=null) {
             setLog("film nie je null "+film.getNazov());
         }
